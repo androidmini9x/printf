9a0a8f86af5c421b8cc7d2ca13aae89ba63efd77
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-01-21 21:13:56 +0200

    Update op_uint.c

--- op_uint.c
+++ op_uint.c
@@ -13,7 +13,11 @@ int print_unint(va_list ap)
 	while (number / divider > 9)
 		divider *= 10;
 	while (divider != 0)
-	
+	{
+		count += _putchar('0' + (number / divider));
+		number %= divider;
+		divider /= 10;
+	}
 
 	return (count);
 }
