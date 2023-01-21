ccfccb8b9d20a8e6c80e32e25c0ac255cef1897d
Androidmini9x <androidmini9x@gmail.com>
2023-01-21 21:08:11 +0300

    Add: putschar function

---
+++ puts.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * _putchar - print the character c to stdout
+ * @c: The character to print
+ * Return: 1 or -1 if failed
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
