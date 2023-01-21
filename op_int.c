6642956cedd6db04b8878fe6dc7a3eb9fe0a4ccf
Androidmini9x <androidmini9x@gmail.com>
2023-01-21 21:09:00 +0300

    Add: integer function

---
+++ op_int.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_int - print integer
+ * @ap: pointer
+ * Return: count of printed length
+ */
+int print_int(va_list ap)
+{
+	unsigned int number;
+	unsigned int divider = 1;
+	int count = 0;
+	int arg = va_arg(ap, int);
+
+	if (arg < 0)
+	{
+		_putchar('-');
+		count++;
+		number = -1 * arg;
+	}
+	else
+		number = arg;
+
+	while (number / divider > 9)
+		divider *= 10;
+	while (divider != 0)
+	{
+		count += _putchar('0' + (number / divider));
+		number %= divider;
+		divider /= 10;
+	}
+
+	return (count);
+}
