427b1a7424c82db7b939470eb1095486275d1c28
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-01-23 01:28:06 +0200

    Update main.h

--- main.h
+++ main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 #include <stddef.h>
 #include <stdarg.h>
-int get_flags(const char *format, int *i);
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list ap);
@@ -19,30 +18,6 @@ int print_strhex(va_list ap);
 int print_ptr(va_list ap);
 int print_rstr(va_list ap);
 int print_r13(va_list ap);
-/* get_flag */
-int get_flag(char s, flags_t *f);
-void handle_flags(char flag)
-void init_params(params_t *params, va_list ap)	
-	unsigned int plus_flag		: 1;
-	unsigned int space_flag		: 1;
-	unsigned int hashtag_flag	: 1;
-	unsigned int zero_flag		: 1;
-	unsigned int minus_flag		: 1;
-unsigned char handle_flags(const char *flag, char *index);
-unsigned char handle_length(const char *modifier, char *index);
-int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
-unsigned int convert_di(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_b(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_u(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_o(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-
 /**
  * struct op - operation
  * @op_type: print type
