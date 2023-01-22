968acfbf83d8b7e74e6593497a5cbf3b377f94cd
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-01-22 09:48:50 +0200

    Update get_flag.c.c

--- get_flag.c.c
+++ get_flag.c.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
 /**
- * init_params - clears struct fields and reset buf
- * @params: the parameters struct
- * @ap: the argument pointer
- * Return: void
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
  */
-
-void init_params(params_t *params, va_list ap)
+int get_flags(const char *format, int *i)
 {
-	params->unsign = 0;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	params->plus_flag = 0;
-	params->space_flag = 0;
-	params->hashtag_flag = 0;
-	params->zero_flag = 0;
-	params->minus_flag = 0;
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
 
-	params->width = 0;
-	params->precision = UINT_MAX;
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
 
-	params->h_modifier = 0;
-	params->l_modifier = 0;
-	(void)ap;
-}     
+	*i = curr_i - 1;
+
+	return (flags);
+}
